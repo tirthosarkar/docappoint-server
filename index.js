1586895561c4f3f3fb4917bc78586e3028b02e9b
tirthosarkar <tirthosarkar1@gmail.com>
2026-05-27 10:49:04 +0600

    authorization

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const app = express();
 const cors = require('cors');
+const { createRemoteJWKSet, jwtVerify } = require('jose-cjs');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 dotenv.config();
 
@@ -10,7 +11,11 @@ const port = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.MONGODB_URL;
+const uri = process.env.MONGODB_URI;
+
+const JWKS = createRemoteJWKSet(
+  new URL(`${process.env.CLIENT_URL}/api/auth/jwks`),
+);
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -20,27 +25,67 @@ const client = new MongoClient(uri, {
   },
 });
 
+const logger = (req, res, next) => {
+  console.log(req.params, 'from second');
+  console.log(`${req.method} | ${req.url}`);
+  next();
+};
+
+const verifyToken = async (req, res, next) => {
+  const { authorization } = req.headers;
+  // console.log(req.headers, 'from verify token');
+  const token = authorization?.split(' ')[1];
+  //console.log(token);
+
+  if (!token) {
+    return res.status(401).json({ message: 'Unauthorize' });
+  }
+
+  try {
+    const JWKS = createRemoteJWKSet(
+      new URL('http://localhost:3000/api/auth/jwks'),
+    );
+    const { payload } = await jwtVerify(token, JWKS);
+    req.user = payload;
+
+    next();
+  } catch (error) {
+    console.error('Token validation failed:', error);
+    return res.status(401).json({ message: 'Unauthorize' });
+  }
+};
+
 async function run() {
   try {
-    // await client.connect();
+    //await client.connect();
 
     const db = client.db('docappointdb');
     const appointmentCollection = db.collection('appointment');
 
     app.get('/appointment', async (req, res) => {
+      const { search } = req.query;
+
+      let cursor;
+      if (search) {
+        cursor = appointmentCollection.find({ name: search });
+      } else {
+        cursor = appointmentCollection.find();
+      }
+
       const cursor = appointmentCollection.find();
       const result = await cursor.toArray();
+      console.log(result);
       //console.log(result);
       res.send(result);
     });
 
- app.get('/featured', async (req, res) => {
-   const cursor = appointmentCollection.find().limit(3);
-   const result = await cursor.toArray();
-   res.send(result);
- }); 
+    app.get('/featured', async (req, res) => {
+      const cursor = appointmentCollection.find().limit(3);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-    app.get('/appointment/:appintId', async (req, res) => {
+    app.get('/appointment/:appintId', logger, verifyToken, async (req, res) => {
       const { appintId } = req.params;
       const query = { _id: new ObjectId(appintId) };
       const result = await appointmentCollection.findOne(query);
